01a2e31d43b133d51fb09804d9ca8e864988a4b9
Ernesto Ramiro <atomms@gmail.com>
2021-10-04 18:47:16 +0200

    resources up

---
+++ app/src/main/java/com/ramiro/ernesto/home/Constraining.java
@@ -0,0 +1,20 @@
+package com.ramiro.ernesto.home;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * first class
+ * @author ernesto
+ */
+public class Constraining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraining);
+
+    }
+}
